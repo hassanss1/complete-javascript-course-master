3211309b171b6926531a547f468c658c3f36fa72
hassan <hassanssaade@gmail.com>
2021-11-08 06:20:02 -0300

    code refactoring and fixed highscore after new game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -15,9 +15,19 @@
 }
 // Defining secretNumber
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
 // Defining starting score
 let score = 20;
 let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const changeBackground = function (color) {
+  document.querySelector('body').style.backgroundColor = color;
+};
+const setHighscore = function (highscore) {
+  document.querySelector('.highscore').textContent = highscore;
+};
 
 // Listening to buttons beign clicked
 document.querySelector('.check').addEventListener('click', function () {
@@ -25,52 +35,41 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guess, typeof guess);
   // If the guess is empty and the person just clicked the button?
   if (!guess) {
-    document.querySelector('.message').textContent = 'No value inserted! ';
+    displayMessage('No value inserted!');
     // If the guess number is equal to the secretNumber
   } else if (guess > 20) {
-    document.querySelector('.message').textContent =
-      'Duuh it is between 1 and 20!';
+    displayMessage('Duuh it is between 1 and 20!');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent =
-      '🎊 You have found the number! 🥳';
-    document.querySelector('body').style.backgroundColor = '#60b347';
+    displayMessage('🎊 You have found the number! 🥳');
+    changeBackground('#60b347');
     document.querySelector('.number').style.width = '30rem';
     if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = score;
+      setHighscore(highscore);
     }
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.check').disabled = true;
     // If the guess number is below the secretNumber
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-    // If the guess number is above the secretNumber
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
+  } else if (guess !== secretNumber) {
+    displayMessage(guess < secretNumber ? '📉 Too low!' : '📈 Too high!');
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    displayMessage('💥 You lost the game!');
+    document.querySelector('.score').textContent = 0;
   }
 });
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('body').style.backgroundColor = '#222';
+  changeBackground('#222');
   document.querySelector('.guess').value = '';
+  score = 20;
   document.querySelector('.score').textContent = '20';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.number').style.width = '15rem';
   secretNumber = Math.trunc(Math.random() * 20 + 1);
   document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').textContent = secretNumber;
+  document.querySelector('.check').disabled = false;
   // Keeping record of previous highscore
-  if (!highscore) document.querySelector('.highscore').textContent = highscore;
+  if (!highscore) setHighscore(highscore);
 });
