a2296f56f1dd49c4273f946a1b24fb493ac07e5f
hassan <hassanssaade@gmail.com>
2022-06-25 12:18:36 +0100

    starting async class

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,17 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+// Old-school XMLHttpRequest way
+const request = new XMLHttpRequest();
+
+// public-apis on github CORS need to be yes to access it from our own code
+// API ENDPOINTS (url that we need)
+request.open('GET', 'https://restcountries.com/v2/name/portugal');
+// to send off the request to fetch data on the request
+request.send();
+
+request.addEventListener('load', function () {
+  // as soon as the load finished and data arrives this function runs
+  console.log(this.responseText);
+});
